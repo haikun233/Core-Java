e22f0e2b8c8bb673b45f12acc56134d1d09d8d44
haikun233 <15189798081@163.com>
2018-07-10 10:21:37 +0800

    version 1.01

---
+++ Constants2.java
@@ -0,0 +1,13 @@
+/**
+ * @version 1.01 2018-07-10
+ * @author William Chang
+ */
+public class Constants2
+{
+	public static final double CM_PER_INCH = 2.54;
+	public static void main(String[] args) {
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+	}
+}
